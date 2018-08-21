8e225d6120a9c441e6a0e858b27b16b6f7b2b69a
Панов Илья <panov210@yandex.ru>
2018-08-21 15:43:11 +0300

    Delete the TextBox. Added instead Image::drawTextBox (completely rewrote the algorithm).

--- src/Image.php
+++ src/Image.php
@@ -8,6 +8,9 @@ use GD\Structures\Rect;
 use InvalidArgumentException;
 
 class Image {
+    const ALIGN_START = -1, ALIGN_CENTER = 0, ALIGN_END = 1;
+    const ALIGN_MULTIPLIERS = [self::ALIGN_START => 0.1, self::ALIGN_CENTER => 0.5, self::ALIGN_END => 0.9];
+
     /** @var resource $handle */
     private $handle;
 
@@ -116,6 +119,24 @@ class Image {
                 $text
             ) !== false;
     }
+
+    public function drawTextBox(string $text, Rect $rect, int $horizontalAlign = self::ALIGN_CENTER, int $verticalAlign = self::ALIGN_CENTER) {
+        $sourceFontSize = $fontSize = $this->fontSize;
+        $lineWidth = (int)round($rect->size->width / $sourceFontSize);
+        $text = wordwrap(html_entity_decode($text), $lineWidth);
+
+        do {
+            $computedBox = Font::computeTextBox($text, $this->fontFilename, $fontSize--, $this->fontRotation);
+        } while ($computedBox->size->height > $rect->size->height);
+
+        $fontSize++;
+        $x = $rect->location->x + ($rect->size->width - $computedBox->size->width) * self::ALIGN_MULTIPLIERS[$horizontalAlign];
+        $y = $rect->location->y + $fontSize + ($rect->size->height - $computedBox->size->height) * self::ALIGN_MULTIPLIERS[$verticalAlign];
+
+        $this->fontSize = $fontSize;
+        $result = $this->drawText($text, Point::of($x, $y));
+        $this->fontSize = $sourceFontSize;
+        return $result;
     }
 
     public function drawRectangle(Rect $rect, Color $color, bool $fill = false): bool {
